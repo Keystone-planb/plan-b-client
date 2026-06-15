00ec58a45e71ab50008a807f5f9ec73766c7e931
Hyeeun <78162567+khy0819@users.noreply.github.com>
2026-06-15 11:34:14 +0900

    fix: Android Google Maps 설정 정리

--- app.config.js
+++ app.config.js
@@ -1,40 +1,46 @@
 import "dotenv/config";
 
-export default ({ config }) => {
-  return {
-    ...config,
+export default ({ config }) => ({
+  ...config,
 
-    scheme: "planb",
+  name: "PLAN.B",
+  slug: "keystone",
+  scheme: "planb",
 
-    android: {
-      ...config.android,
-      package: "com.planbtravel.app",
-      config: {
-        ...config.android?.config,
-        googleMaps: {
-          apiKey:
-            process.env.EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_API_KEY ||
-            "AIzaSy...실제_구글_지도_키_직접_입력",
-        },
+  android: {
+    ...config.android,
+    package: "com.planbtravel.app",
+    config: {
+      ...(config.android?.config ?? {}),
+      googleMaps: {
+        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_API_KEY ?? "",
       },
     },
+  },
 
-    ios: {
-      ...config.ios,
-      bundleIdentifier: "com.planbtravel.app",
-      config: {
-        usesNonExemptEncryption: false,
-      },
-      config: {
-        ...config.ios?.config,
-      },
+  ios: {
+    ...config.ios,
+    bundleIdentifier: "com.planbtravel.app",
+    config: {
+      ...(config.ios?.config ?? {}),
+      usesNonExemptEncryption: false,
     },
+    infoPlist: {
+      ...(config.ios?.infoPlist ?? {}),
+      ITSAppUsesNonExemptEncryption: false,
+      LSApplicationQueriesSchemes: [
+        "kakaokompassauth",
+        "storykompassauth",
+        "kakaolink",
+        "kakaotalk",
+      ],
+    },
+  },
 
-    extra: {
-      ...config.extra,
-      eas: {
-        projectId: "ae4365b2-a5ee-46b6-b12c-e4ef3fd10f7a",
-      },
+  extra: {
+    ...(config.extra ?? {}),
+    eas: {
+      projectId: "ae4365b2-a5ee-46b6-b12c-e4ef3fd10f7a",
     },
-  };
-};
+  },
+});
